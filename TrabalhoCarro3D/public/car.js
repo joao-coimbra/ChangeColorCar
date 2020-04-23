25ba2523d29b7426fe7cb47f4d8add0e44ff0df1
Felipe Alvarez dos Santos <alvarezfelipe_@hotmail.com>
2020-04-22 21:15:46 -0300

    inclusão tipo de roda
    inclusão da alteração do tipo da roda "Change Wheels"

--- TrabalhoCarro3D/public/car.js
+++ TrabalhoCarro3D/public/car.js
@@ -2,3 +2,8 @@ function changeColor(color) {
     var material = document.getElementById('body_color');
     material.setAttribute("diffuseColor", color);
   }
+
+ function changeTexture(tex) {
+		document.getElementById('rim_tex').setAttribute("url", tex);
+	}
+  
